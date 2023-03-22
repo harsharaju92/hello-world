66fd7ff75820cfde1d20ab154c4f9efb2fffe360
harsha <104590330+harsharaju92@users.noreply.github.com>
2023-03-22 19:17:04 +0530

    checking on kmaster server

--- createlocal.sh
+++ createlocal.sh
@@ -1,8 +1,8 @@
 #runmqsc QM3 < qlocal.mqsc
 echo "Hello Madhuri"
 sudo su 
-sudo su - ansible
-ssh ubuntu@172.31.38.144
-sudo su 
+#sudo su - ansible
+#ssh ubuntu@172.31.38.144
+#sudo su 
 su - mqm
 runmqsc QM1 < qlocal.txt
