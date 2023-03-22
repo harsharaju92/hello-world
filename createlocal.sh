3ec39eb1fc8bcf86b603e2bc291a661505601fe5
harsha <104590330+harsharaju92@users.noreply.github.com>
2023-03-22 19:21:02 +0530

    changed the script

--- createlocal.sh
+++ createlocal.sh
@@ -1,8 +1,8 @@
 #runmqsc QM3 < qlocal.mqsc
 echo "Hello Madhuri"
-sudo su 
+sudo su - mqm 
 #sudo su - ansible
 #ssh ubuntu@172.31.38.144
 #sudo su 
-su - mqm
-runmqsc QM1 < qlocal.txt
+#su - mqm
+runmqsc QM1 < qlocal.mqsc
